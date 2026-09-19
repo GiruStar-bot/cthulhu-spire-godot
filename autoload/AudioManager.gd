faf177d38b50e4d429c99798e73304e2c5ebf391
SOICHIRO <a5402s2004@gmail.com>
2026-09-19 15:24:53 +0800

    feat(audio): map gift_talk / gift_reveal cues

--- autoload/AudioManager.gd
+++ autoload/AudioManager.gd
@@ -32,6 +32,8 @@ const SFX_PATHS := {
 	"paper_draw": "res://audio/sfx/sfx_paper_draw.wav",
 	"gift_open": "res://audio/sfx/sfx_gift_open.wav",
 	"gift_confirm": "res://audio/sfx/sfx_gift_confirm.wav",
+	"gift_talk": "res://audio/sfx/sfx_gift_talk.wav",
+	"gift_reveal": "res://audio/sfx/sfx_gift_reveal.wav",
 	"vfx_impact": "res://audio/sfx/sfx_vfx_impact.wav",
 	"vfx_slash": "res://audio/sfx/sfx_vfx_slash.wav",
 	"vfx_arrow": "res://audio/sfx/sfx_vfx_arrow.wav",
